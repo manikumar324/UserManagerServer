55b47cacf105d6761237e78a2e65762d8c903dc4
ManiKumar Nakka <manikumarnakka324@gmail.com>
2024-10-21 16:46:41 +0530

    errors fixed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(cors())
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use("/uploads", express.static(path.join(__dirname, "utils", "uploads")));
+  app.use("/uploads", express.static(path.join(__dirname, "Utils", "uploads")));
   
   mongoose.connect(DBurl, {
     useNewUrlParser: true,
